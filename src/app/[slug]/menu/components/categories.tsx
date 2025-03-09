7e51202ee2d6a97ec920c22062eec3e7cab6f01c
GiselleBalieiro <gisellefbalieiro@outlook.com>
2025-03-09 16:52:33 -0300

    feat: add shopping cart component and order list

--- src/app/[slug]/menu/components/categories.tsx
+++ src/app/[slug]/menu/components/categories.tsx
@@ -3,10 +3,14 @@
 import { Prisma } from "@prisma/client";
 import { ClockIcon } from "lucide-react";
 import Image from "next/image";
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { formatCurrency } from "@/helpers/format-currency";
+
+import { CartContext } from "../contexts/cart";
+import CartSheet from "./cart-sheet";
 import Products from "./products";
 
 interface RestaurantCategoriesProps {
@@ -26,17 +30,14 @@ type MenuCategoriesWithProducts = Prisma.MenuCategoryGetPayload<{
 const RestaurantCategories = ({ restaurant }: RestaurantCategoriesProps) => {
   const [selectedCategory, setSelectedCategory] =
     useState<MenuCategoriesWithProducts>(restaurant.menuCategories[0]);
-
+  const { products, total, toggleCart, totalQuantity } =
+    useContext(CartContext);
   const handleCategoryClick = (category: MenuCategoriesWithProducts) => {
     setSelectedCategory(category);
   };
-
-  const getCategoryButtonClasses = (category: MenuCategoriesWithProducts) => {
-    return selectedCategory.id === category.id
-      ? "bg-yellow-500 text-black"
-      : "bg-white text-black";
+  const getCategoryButtonVariant = (category: MenuCategoriesWithProducts) => {
+    return selectedCategory.id === category.id ? "default" : "secondary";
   };
-
   return (
     <div className="relative z-50 mt-[-1.5rem] rounded-t-3xl bg-white">
       <div className="p-5">
@@ -64,8 +65,9 @@ const RestaurantCategories = ({ restaurant }: RestaurantCategoriesProps) => {
             <Button
               onClick={() => handleCategoryClick(category)}
               key={category.id}
+              variant={getCategoryButtonVariant(category)}
               size="sm"
-              className={`rounded-full ${getCategoryButtonClasses(category)}`}
+              className="rounded-full"
             >
               {category.name}
             </Button>
@@ -76,6 +78,21 @@ const RestaurantCategories = ({ restaurant }: RestaurantCategoriesProps) => {
 
       <h3 className="px-5 pt-2 font-semibold">{selectedCategory.name}</h3>
       <Products products={selectedCategory.products} />
+      {products.length > 0 && (
+        <div className="fixed bottom-0 left-0 right-0 flex w-full items-center justify-between border-t bg-white px-5 py-3">
+          <div>
+            <p className="text-xs text-muted-foreground">Total dos pedidos</p>
+            <p className="text-sm font-semibold">
+              {formatCurrency(total)}
+              <span className="text-xs font-normal text-muted-foreground">
+                / {totalQuantity} {totalQuantity > 1 ? "itens" : "item"}
+              </span>
+            </p>
+          </div>
+          <Button onClick={toggleCart}>Ver sacola</Button>
+          <CartSheet />
+        </div>
+      )}
     </div>
   );
 };
