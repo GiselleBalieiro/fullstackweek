f6a7d609557e1688fa4d45969fe8f050d87919ed
GiselleBalieiro <gisellefbalieiro@outlook.com>
2025-03-09 17:32:14 -0300

    chore: remove commented lines

--- src/app/[slug]/menu/actions/create-order.ts
+++ src/app/[slug]/menu/actions/create-order.ts
@@ -6,6 +6,8 @@ import { revalidatePath } from "next/cache";
 import { db } from "@/lib/prisma";
 
 import { removeCpfPunctuation } from "../helpers/cpf";
+import { redirect } from "next/navigation";
+
 
 interface CreateOrderInput {
   customerName: string;
@@ -58,8 +60,7 @@ export const createOrder = async (input: CreateOrderInput) => {
     },
   });
   revalidatePath(`/${input.slug}/orders`);
-  // redirect(
-  //   `/${input.slug}/orders?cpf=${removeCpfPunctuation(input.customerCpf)}`,
-  // );
+
+  redirect(`/${input.slug}/orders?cpf=${removeCpfPunctuation(input.customerCpf)}`);
   return order;
 };
